ca0f602a92eefeed6630e6ba322c3fd76b58320a
Jin Yu <jinyu@microsoft.com>
2025-06-26 06:25:13 +0000

    add dataverse entity reference links

--- src/query_dataverse.py
+++ src/query_dataverse.py
@@ -31,6 +31,7 @@ client = DataverseClient(session=session, environment_url=environment_url)
 # ------------------------------------------------------------------------
 
 # Instantiate interface to the entity
+# https://learn.microsoft.com/en-us/power-apps/developer/data-platform/webapi/reference/bot_botcomponent
 entity = client.entity(logical_name="botcomponent")
 
 # Read data from the entity
@@ -39,9 +40,10 @@ data = entity.read(
     filter=f"botcomponentid eq '{os.getenv("BOT_COMPONENT_ID")}'",
 )[0]
 
-print(f"Name: {data['name']}")
-print(f"Description: {data['description']}")
-print(f"Specification: {data['data']}")
+print("\n------------------------- Bot Component -------------------------")
+print(f"\nName: {data['name']}")
+print(f"\nDescription: {data['description']}")
+print(f"\nSpecification: {data['data'][:1500]}\n...")
 
 
 # --------------------------------------------
@@ -49,6 +51,7 @@ print(f"Specification: {data['data']}")
 # --------------------------------------------
 
 # Instantiate interface to the entity
+# https://learn.microsoft.com/en-us/power-apps/developer/data-platform/webapi/reference/conversationtranscript
 entity = client.entity(logical_name="conversationtranscript")
 
 # Read data from the entity
@@ -65,18 +68,20 @@ data = entity.read(
     filter=f"contains(name, '{os.getenv("CONVERSATION_ID")}')",
 )
 
+
+print("\n\n------------------------- Conversation Logs -------------------------")
 for item in data:
     item["metadata"] = json.loads(item["metadata"])
     print(f"\nBot : {item['metadata']['BotName']}")
-    print(f"Bot ID: {item['_bot_conversationtranscriptid_value']}")
+    print(f"\nBot ID: {item['_bot_conversationtranscriptid_value']}")
     # Name of conversation is prefixed with Conversation ID
-    print(f"Name of Conversation: {item['name']}")
-    print(f"Transcript ID: {item['conversationtranscriptid']}")
-    print(f"Conversation Start Time: {item['conversationstarttime']}")
-    print(f"Record created On: {item['createdon']}")
+    print(f"\nName of Conversation: {item['name']}")
+    print(f"\nTranscript ID: {item['conversationtranscriptid']}")
+    print(f"\nConversation Start Time: {item['conversationstarttime']}")
+    print(f"\nRecord created On: {item['createdon']}")
     # Content field contains conversation logs/activities
     content = json.loads(item["content"])
-    print(f"Conversation Log: {json.dumps(content, indent=2)}")
+    print(f"\nConversation Log: {json.dumps(content, indent=2)[:1500]}\n...")
 
 # Close the client session
 session.close()
